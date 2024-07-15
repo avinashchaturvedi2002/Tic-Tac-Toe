853bc29de7a36896a841688da65cff27f7a0fb95
avinashchaturvedi2002 <avinashchaturvedi2002@gmail.com>
2024-07-15 09:21:58 +0530

    Fully functional game has been developed

---
+++ script.js
@@ -0,0 +1,64 @@
+let boxes=document.querySelectorAll(".marker");
+let turnO=true;
+let winnerText=document.querySelector(".winnerText")
+let rstBtn=document.querySelector("#reset")
+
+const winPattern=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+boxes.forEach((box)=>
+{
+    box.addEventListener("click",()=>{
+        if (turnO)
+        {
+            box.innerText="0";
+            turnO=false;
+            
+        }
+        else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+
+        checkWinner();
+    })
+})
+
+const checkWinner=()=>{
+    for(let pattern of winPattern)
+    {
+        if((boxes[pattern[0]].innerText!="")&&(boxes[pattern[1]].innerText!="")&&(boxes[pattern[2]].innerText!=""))
+        {
+            if((boxes[pattern[0]].innerText===boxes[pattern[1]].innerText)&&(boxes[pattern[1]].innerText===boxes[pattern[2]].innerText))
+            {
+            winnerText.innerText=`Winner is ${boxes[pattern[0]].innerText}`;
+            disable();
+
+            }
+        }
+    }
+
+}
+
+const disable=()=>{
+    boxes.forEach((box)=>{
+        box.disabled=true;
+    })
+}
+
+const enable=()=>{
+    boxes.forEach((box)=>{
+        box.disabled=false;
+    })
+}
+
+rstBtn.addEventListener("click",()=>{
+    boxes.forEach((box)=>{
+        box.innerText="";
+    })
+    enable();
+    winnerText.innerText="";
+})
+
+
+
